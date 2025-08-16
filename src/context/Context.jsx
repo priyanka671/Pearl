a8bce5fd2bde6cf184dd7be74e01a541b2be8e7f
Priyanka <padhypriyanka69@gmail.com>
2025-08-16 21:13:46 +0530

    Setup deplyment to github pages

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -13,6 +13,11 @@ const ContextProvider = (props) => {
       const [loading,setLoading] = useState(false);
       const [resultData, setResultData] = useState("");
 
+      const delaypara = (index,nextWorld) => {
+
+      }
+
+
         const onSent = async (prompt) => {
 
             setResultData("")
@@ -20,7 +25,19 @@ const ContextProvider = (props) => {
             setShowResult(true)
             setRecentPrompt(input)
             const response = await  runChat(input)
-            setResultData(response)
+            let responseArray = response.split("**");
+            let newResponse ;
+            for(let i=0; i< responseArray.length; i++)
+            {
+                  if(i==0 || i%2 !==1){
+                        newResponse += responseArray[i];
+                  }
+                  else{
+                        newResponse += "<b>" + responseArray[i] + "</b>";
+                  }
+            }
+            let newResponse2 = newResponse.split("*").join("</br>");
+            setResultData(newResponse)
             setLoading(false)
             setInput(" ")
         }
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {HashRouter} from 'react-router-dom'
+
 import App from './App.jsx'
 import ContextProvider from './context/context'
 
 createRoot(document.getElementById('root')).render(
     
-    <ContextProvider>
-        <HashRouter basename="/pearl">
-            <App />
-        </HashRouter>
-    </ContextProvider>
+    
+            <ContextProvider>
+                <App />
+            </ContextProvider>
+        
+
 )
 
