bd3cc3a8d9aaa206849c930c2aa8f6e18b656ca4
Priyanka <padhypriyanka69@gmail.com>
2025-08-16 20:04:10 +0530

    deployment to Github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import {HashRouter} from 'react-router-dom'
 import App from './App.jsx'
 import ContextProvider from './context/context'
 
 createRoot(document.getElementById('root')).render(
     
     <ContextProvider>
-       <App />
+        <HashRouter basename="/pearl">
+            <App />
+        </HashRouter>
     </ContextProvider>
 )
 
