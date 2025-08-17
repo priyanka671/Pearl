9db4e9da3307e640497dcf5d0d7016dfad432106
Priyanka <padhypriyanka69@gmail.com>
2025-08-17 20:27:38 +0530

    Some changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 import App from './App.jsx'
-import ContextProvider from './context/context'
+import ContextProvider from './context/Context';
 
 createRoot(document.getElementById('root')).render(
     
